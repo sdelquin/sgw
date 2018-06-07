f14e1509d1cd2137c566b5290ada448abdbbc4d7
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-06-07 21:14:43 +0100

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sgw",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/sdelquin/sgw.git",
     author="Sergio Delgado Quintero",
     author_email="sdelquin@gmail.com",
