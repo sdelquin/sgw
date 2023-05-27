9be3710188d9327dc99d790b745f334422c1fadd
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-05-27 23:17:41 +0100

    Add README to setup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ Releases use [Semantic Versioning](https://semver.org/) (`<major>.<minor>.<patch
 
 Released XXXX-XX-XX
 
+- Add [justfile](./justfile) to automate operations.
+- Add [README](./README.md) to [setup.py](./setup.py).
+
 ## Version 1.2.0
 
 Released 2023-05-27
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
+# read the contents of your README file
+from pathlib import Path
+
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="sgw",
-    version="1.2.0",
+    version="1.2.0.1",
     url="https://github.com/sdelquin/sgw.git",
     author="Sergio Delgado Quintero",
     author_email="sdelquin@gmail.com",
     description="Wrapper made in Python for sendgrid",
     packages=["sgw"],
     install_requires=["sendgrid==5.3.0"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
